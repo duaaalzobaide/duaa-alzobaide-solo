41866fe8b8771c3e35b3ff5e1c4fd6b27ebf0ec5
duaa_alzubaide <justduaa6@gmail.com>
2020-10-08 07:06:11 +0300

    finishedddd

--- index.js
+++ index.js
@@ -1,43 +1,112 @@
+window.onload = function() {
 
-///create vars to hold the time
+    var seconds = 00;
+    var tens = 00;
+    var minutes = 00;
 
-
-var seconds = 0;
-var minutes = 0;
-var hours = 0;
-
-
-$('#st').click(function start() {
-
-	seconds++
-
-	////when to increment next val
-
-	if(seconds/60 ===1){
-
-		seconds=0;
-		minutes++
-
-		if(minutes/60===1)
-
-			minutes = 0;
-	
-
-		    hours++;
-
-
-document.getElementById('#display').innerHTML = hours + ':' + minutes + ':' + seconds
-
-
-
-		}
-	})
-      
-      
-      window.setInterval(stopwatch, 1000)
+    var appendTens = document.getElementById("tens")
+    var appendSeconds = document.getElementById("seconds")
+    var appendMinutes = document.getElementById('minutes')
+    var buttonStart = document.getElementById('button-start');
+    var buttonStop = document.getElementById('button-stop');
+    var buttonReset = document.getElementById('button-reset');
+    var Interval;
+    var laps = document.getElementById('laps');
+    var lapsList = [];
+    var resetLaps = document.getElementById('reset-laps');
 
 
 
 
+    laps.onclick = function() {
+        var text = "";
+
+        var currentTimer = document.getElementById('minutes').innerHTML + ':' + document.getElementById("seconds").innerHTML + ':' + document.getElementById("tens").innerHTML
+
+        if (lapsList.indexOf(currentTimer) !== -1) {
+            return;
+
+        }
+        lapsList.unshift(currentTimer);
 
 
+
+        clearInterval(Interval);
+        text += "<ul>";
+        for (i = 0; i < lapsList.length; i++)  {
+            text += "<li>" + lapsList[i] + "</li>";
+        }
+        text += "</ul>";
+        document.getElementById("lapsList").innerHTML = text;
+    }
+
+
+
+    buttonStart.onclick = function() {
+
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+    }
+
+    buttonStop.onclick = function() {
+        clearInterval(Interval);
+    }
+
+
+    buttonReset.onclick = function() {
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
+        minutes = "00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+        appendMinutes.innerHTML = minutes;
+    }
+
+
+    resetLaps.onclick = function() {
+        lapsList = [];
+        document.getElementById("lapsList").innerHTML = "";
+    }
+
+
+
+    function startTimer() {
+        tens++;
+
+        if (tens < 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+
+        if (tens > 9) {
+            appendTens.innerHTML = tens;
+
+        }
+
+        if (tens > 99) {
+            
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+
+        }
+
+        if (seconds > 9) {
+            appendSeconds.innerHTML = seconds;
+        }
+
+        if(seconds>59){
+          minutes++;
+          tens = 0;
+          appendTens.innerHTML = "0" + 0;
+          seconds = 0
+          appendSeconds.innerHTML = "0" + 0;
+          appendMinutes.innerHTML = minutes;
+
+        }
+
+    }
+
+
+}
