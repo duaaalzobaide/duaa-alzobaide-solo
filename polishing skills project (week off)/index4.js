a14b2dece37b49b0cfd16c096f8779e3b8f857b3
duaa_alzubaide <justduaa6@gmail.com>
2020-10-12 18:22:38 +0300

    step3

--- polishing skills project (week off)/index4.js
+++ polishing skills project (week off)/index4.js
@@ -1,14 +1,34 @@
 var users = []
 
+var singleUser = {}
+
 console.log(users)
 
 document.getElementById('push').onclick = function(){
 
-	var user = document.getElementsByClassName('form-control').innerHTML
+///so i think i have to put each user in an object and push the object to the array
 
-	users.push(user)
+	singleUser.name = document.getElementById('name').innerHTML
+    singleUser.phone = document.getElementById('phone').innerHTML
+    singleUser.pass = document.getElementById('pass1').innerHTML
+    singleUser.confirmation = document.getElementById('pass2').innerHTML
+
+
+	users.push(singleUser)
 
 	console.log(users)
 
 
+}
+
+// here in the login page , i want to check if the password exists and true , if it is , then log in and go
+//to the intended page .
+document.getElementById('log').onclick = function(){
+
+	for(var i = 0;i<users.length;)
+
+	if(document.getElementById('exampleInputPassword') === users[i].pass){
+
+
+	}
 }
